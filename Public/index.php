71e67d6699c95604361c81e3759af9873f379f34
bellos <bellos@intralot.com>
2016-05-11 12:01:27 +0300

    - Set include path from composer

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,7 @@
 <?php
 require('../vendor/autoload.php');
-set_include_path("." . PATH_SEPARATOR . realpath(__DIR__."/../"));
 use Edge\Core\Edge;
+//the path below is resolved due to the include path setting in composer.json
 $webApp = new Edge("Application/Config/config.php");
 $router = $webApp->getConfig('routerClass');
 (new $router($webApp->getRoutes()))->invoke();
